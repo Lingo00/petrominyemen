6f3ae14058bf47aa4c2233b9010d761597c20577
Lingo00 <71224840+Lingo00@users.noreply.github.com>
2024-11-19 07:20:29 +0300

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "petromin";
 
+// الاتصال بقاعدة البيانات
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// تحقق من الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال: " . $conn->connect_error);
+}
+?>
